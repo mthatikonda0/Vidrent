979ba3b98e25cd3c9086ca3105edc61a8ecad6ad
mthatikonda0 <mounish.eceb@gmail.com>
2017-04-12 16:59:20 -0400

    building an api for customers
    Restful Apis

--- Vidrent/Global.asax.cs
+++ Vidrent/Global.asax.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using AutoMapper;
+using Vidrent.App_Start;
 
 namespace Vidrent
 {
@@ -12,6 +15,8 @@ namespace Vidrent
     {
         protected void Application_Start()
         {
+            Mapper.Initialize( C => C.AddProfile<MappingProfile>());
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
